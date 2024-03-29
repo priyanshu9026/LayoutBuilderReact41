c1d07d593aadb7c7e8c134660bb652f1e8ce8880
priyanshu9026 <singpriyasnshu22@gmail.com>
2024-03-29 13:13:30 +0000

    initial commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,9 @@
+// Write your code here
+import './index.css'
+
+const Footer = () => (
+  <div className="header-heading">
+    <h1 className="header">Footer</h1>
+  </div>
+)
+export default Footer
---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+// Write your code here
+import './index.css'
+
+const Header = () => (
+  <div className="header-heading">
+    <h1 className="header">Header</h1>
+  </div>
+)
+export default Header
